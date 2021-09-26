e0b6bf2744773ae86d69f11d8381c2a5da3d1a87
wongtawan-j <work@wongtawan.dev>
2021-09-26 18:13:14 +0700

    update logging

--- src/datasource/mysql.ts
+++ src/datasource/mysql.ts
@@ -1,21 +1,31 @@
 import {Connection, createConnection} from "typeorm";
+import {ApplicationLogger} from "groupo-shared-service/logging/logger";
 
 let connection: Connection | null = null;
 
+let logger = new ApplicationLogger();
+
 export const getConnection = async (): Promise<Connection> => {
+    logger = logger.field("host", process.env.MYSQL_HOST).field("user", process.env.MYSQL_USER);
+
     if (connection === null) {
-        connection = await createConnection({
-            type: "mysql",
-            host: process.env.MYSQL_HOST,
-            port: parseInt(process.env.MYSQL_PORT, 10) || 3306,
-            username: process.env.MYSQL_USER || "root",
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DB,
-            entities: [__dirname + "/../models/*.ts"],
-            synchronize: true,
-            logging: false,
-        });
-        console.log(`connect to database successfully (host=${process.env.MYSQL_HOST})`);
+        logger.info("connecting to mysql");
+        try {
+            connection = await createConnection({
+                type: "mysql",
+                host: process.env.MYSQL_HOST,
+                port: parseInt(process.env.MYSQL_PORT, 10) || 3306,
+                username: process.env.MYSQL_USER || "root",
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_DB,
+                entities: [__dirname + "/../models/*.ts"],
+                synchronize: true,
+                logging: false,
+            });
+        } catch (err) {
+            logger.error(err.message);
+        }
+        logger.info("connect to mysql successfully");
     }
     return connection;
 };
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,18 @@ import express from "express";
 import routes from "./routes/index";
 import cors from 'cors';
 import {handler} from "groupo-shared-service/apiutils/errors";
+import {ApplicationLogger} from "groupo-shared-service/logging/logger";
 
 const app = express();
+const logger = new ApplicationLogger();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(routes);
-
 app.use(handler);
 
 const port = process.env.APP_PORT || "8080";
 app.listen(port, () => {
-    console.log("Started groupo-user successfully");
-    console.log(`groupo-user is running on port ${port}`);
+    logger.info("start groupo-user successfully");
+    logger.field("application-port", port).info("groupo-user is running");
 });
