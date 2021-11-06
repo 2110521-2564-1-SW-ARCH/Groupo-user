d1a91c29193f62e927d3c54d83e682dbb834b825
wongtawan-j <work@wongtawan.dev>
2021-11-07 05:09:32 +0700

    apply new share service

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -9,7 +9,7 @@ import {
 } from "groupo-shared-service/apiutils/messages";
 import * as ProfileService from "../services/profile.service";
 import {StatusCodes} from "http-status-codes";
-import {verifyAuthorizationHeader} from "groupo-shared-service/services/authentication";
+import {getExpressRequestContext} from "groupo-shared-service/services/express";
 
 export const register = catcher(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const {firstName, lastName, email, password} = req.body as RegisterRequest;
@@ -21,20 +21,20 @@ export const register = catcher(async (req: express.Request, res: express.Respon
 });
 
 export const getProfile = catcher(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const {email} = verifyAuthorizationHeader(req);
+    const ctx = getExpressRequestContext(req);
 
-    const response = await ProfileService.getProfile(email);
+    const response = await ProfileService.getProfile(ctx);
 
     json(res, newAPIResponse<ProfileResponse>(StatusCodes.OK, response));
     next();
 });
 
 export const updateProfile = catcher(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const {email} = verifyAuthorizationHeader(req);
+    const ctx = getExpressRequestContext(req);
 
     const {firstName, lastName} = req.body as UpdateProfileRequest;
 
-    await ProfileService.updateProfile(email, firstName, lastName);
+    await ProfileService.updateProfile(ctx, firstName, lastName);
 
     json(res, newAPIResponse<string>(StatusCodes.OK, ""));
     next();
--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -2,6 +2,7 @@ import {UserCredentials} from "../models/user-credentials.model";
 import {UserProfile} from "../models/user-profile.model";
 import {ProfileResponse} from "groupo-shared-service/apiutils/messages";
 import {getConnection, Repository} from "typeorm";
+import {ExpressRequestCtx} from "groupo-shared-service/types/express";
 
 const userCredentialsRepository = (): Repository<UserCredentials> => {
     return getConnection().getRepository(UserCredentials);
@@ -23,13 +24,13 @@ export const register = async (firstName: string, lastName: string, email: strin
     await userProfileRepository().insert(profile);
 };
 
-export const getProfile = async (email: string): Promise<ProfileResponse> => {
-    const {firstName, lastName} = await getUserProfile(email);
-    return {firstName, lastName, email};
+export const getProfile = async (ctx: ExpressRequestCtx): Promise<ProfileResponse> => {
+    const {firstName, lastName} = await getUserProfile(ctx.email);
+    return {firstName, lastName, email: ctx.email};
 };
 
-export const updateProfile = async (email: string, firstName: string, lastName: string) => {
-    const profile = await getUserProfile(email);
+export const updateProfile = async (ctx: ExpressRequestCtx, firstName: string, lastName: string) => {
+    const profile = await getUserProfile(ctx.email);
     profile.firstName = firstName;
     profile.lastName = lastName;
     await userProfileRepository().save(profile);
