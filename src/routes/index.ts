05fb8c03cba3690feb325af5165fcfbee54598e9
wongtawan-j <work@wongtawan.dev>
2021-11-11 22:27:16 +0700

    add healthcheck

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
-import { Router } from "express";
+import {Router} from "express";
 import AuthRouter from "./auth.router";
 import ProfileRouter from "./profile.router";
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+    res.status(200).json({});
+});
 routes.use('/auth', AuthRouter);
 routes.use('/profile', ProfileRouter);
 
